ea536aeeec97d6c70df94e205318660191f9bdb3
Yenesey2001 <yeneseyclase@gmail.com>
2025-03-17 14:05:46 +0000

    API Estática de Familia Yenesey

--- src/app.py
+++ src/app.py
@@ -15,6 +15,28 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+jhon = {
+    "first_name": "Jhon",
+    "age": 33,
+    "lucky_numbers": [7, 13, 22]
+}
+jane = {
+    "first_name": "Jane",
+    "age": 35,
+    "lucky_numbers": [10, 14, 3]
+}
+
+jimmy = {
+    "first_name": "jimmy",
+    "age": 5,
+    "lucky_numbers": [1]
+}
+
+jackson_family.add_member(jhon)
+jackson_family.add_member(jane)
+jackson_family.add_member(jimmy)
+
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -25,16 +47,64 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+
+
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
+        
         "family": members
     }
 
+
+    return jsonify(response_body), 200
+
+
+
+
+
+@app.route('/members', methods=['POST'])
+def add_new_member():
+
+    request_body = request.json
+
+    if not request_body:
+       return jsonify({"error": "Invalid input"}), 400
+    
+    new_member = jackson_family.add_member(request_body)
+
+    return jsonify(new_member), 200
+
+
+
+
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_member(id):
+
+    result = jackson_family.delete_member(id)    
+    if "done" in result:
+        return jsonify({"msg": "Member deleted"}), 200
+    
+    return jsonify({"Error": "Member not found"}), 404
+
+
+
+
+@app.route('/members/<int:id>', methods=['GET'])
+def get_member(id):
+
+    member = jackson_family.get_member(id)    
+
+    if not member:
+        return jsonify({"Error": "Member not found"}), 404
+    
+    response_body = {
+        "Member": member
+    }
 
     return jsonify(response_body), 200
 
